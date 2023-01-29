ad704d76606794ad6a3c5d96d7cdb0cd9ee63f7a
Dominique Foley <dfoleydev@gmail.com>
2023-01-29 00:06:40 -0500

    Listing page style, interacting with db

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,7 +24,7 @@
 
 <body class="mb-48">
     <nav class="flex justify-between items-center mb-4">
-        <a href="index.html"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
+        <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
             <li>
                 <a href="register.html" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 @include('partials._hero')
+@include('partials._search')
 
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
