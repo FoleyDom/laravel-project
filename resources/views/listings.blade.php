2dc777982fb459b2710259bfb7f8930b02ab2bc4
Dominique Foley <dfoleydev@gmail.com>
2023-01-28 23:21:22 -0500

    UI styling, database created

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,21 +1,51 @@
-<h1>{{$heading}}</h1>
+@extends('layout')
 
-{{-- @if(count($listings) == 0)
+@section('content')
+@include('partials._hero')
+
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
+    {{-- @if(count($listings) == 0)
     <p>No listings found</p>
 @endif --}}
 
+    @unless(count($listings) == 0)
 
-@unless(count($listings) == 0)
+    @foreach ($listings as $listing)
+    <div class="bg-gray-50 border border-gray-200 rounded p-6">
+        <div class="flex">
+            <img class="hidden w-48 mr-6 md:block" src="{{asset('images/no-image.png')}}" alt="" />
+            <div>
+                <h3 class="text-2xl">
+                    <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
+                </h3>
+                <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+                <ul class="flex">
+                    <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                        <a href="#">Laravel</a>
+                    </li>
+                    <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                        <a href="#">API</a>
+                    </li>
+                    <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                        <a href="#">Backend</a>
+                    </li>
+                    <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                        <a href="#">Vue</a>
+                    </li>
+                </ul>
+                <div class="text-lg mt-4">
+                    <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
 
-@foreach ($listings as $listing)
-    <h2>
-        <a href="/listing/{{$listing['id']}}">{{$listing['title']}}</a>
-    </h2>
-    <p>
-        {{$listing['description']}}
-    </p>
-@endforeach
-
-@else
+    @else
     <p>No listing found</p>
-@endunless
+    @endunless
+
+</div>
+
+@endsection
