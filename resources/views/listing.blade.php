0b908eb6b56c6ff8506dd91449170ae9fca5ddff
Dominique Foley <dfoleydev@gmail.com>
2023-01-30 08:33:20 -0500

    Added card component to single listing

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -6,7 +6,7 @@
 <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-    <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+    <x-card class="p-10">
         <div class="flex flex-col items-center justify-center text-center">
             <img class="w-48 mr-6 mb-6" src="{{asset('images/no-image.png')}}" alt="" />
 
@@ -45,7 +45,7 @@
                 </div>
             </div>
         </div>
-    </div>
 </div>
+</x-card>
 
 @endsection
